e86b0f42e809e498e2cd6de3de8dbfe02669553d
epayet <jakspinning@gmail.com>
2014-03-09 23:07:57 +0100

    Created a WorldManager

--- src/com/jak/flappy/FlappyGame.java
+++ src/com/jak/flappy/FlappyGame.java
@@ -19,80 +19,25 @@ import com.jak.flappy.system.SpawnFlappySystem;
 import com.jak.flappy.system.debug.DebugInfoSystem;
 import com.jak.flappy.system.graphic.*;
 import com.jak.flappy.system.GravitySystem;
+import com.jak.flappy.world.WorldManager;
 
 /**
  * Created by manu on 2/19/14.
  */
-public class FlappyGame implements ApplicationListener{
-    private World world;
-    private Array<Disposable> componentsToDispose;
-    private Array<Preparable> componentsToPrepare;
-    private Camera camera;
+public class FlappyGame implements ApplicationListener {
+    private WorldManager worldManager;
 
     @Override
     public void create() {
-        componentsToDispose = new Array<Disposable>();
-        componentsToPrepare = new Array<Preparable>();
-
-        //Libgdx specific
-        LibgdxCameraComponent cameraComponent = new LibgdxCameraComponent(Constants.WORLD_WIDTH, Constants.WORLD_HEIGHT);
-        camera = cameraComponent.getCamera();
-        SpriteBatchComponent spriteBatchComponent = new SpriteBatchComponent();
-        ShapeRendererComponent shapeRendererComponent = new ShapeRendererComponent();
-        BitmapFontComponent bitmapFontComponent = new BitmapFontComponent("calibri.png", "calibri.fnt", "fonts");
-        LibgdxInputComponent inputComponent = new LibgdxInputComponent();
-
-        componentsToDispose.add(bitmapFontComponent);
-        componentsToDispose.add(spriteBatchComponent);
-        componentsToDispose.add(shapeRendererComponent);
-
-        componentsToPrepare.add(shapeRendererComponent);
-        componentsToPrepare.add(spriteBatchComponent);
-
-
-        //world
-        world = new World();
-        world.initialize();
-        world.setSystem(new LibgdxScreenLimitedSystem());
-        world.setSystem(new DrawingSystem());
-        world.setSystem(new GravitySystem());
-        //world.setSystem(new SpawnFlappySystem(2, cameraComponent));
-        world.setSystem(new LibgdxInputSystem());
-        world.setSystem(new LibgdxVelocitySystem());
-        world.setSystem(new DebugInfoSystem());
-        world.setManager(new GroupManager());
-
-        //ninja
-        Entity ninja = world.createEntity();
-        //ninja.addComponent(new RectangleComponent(Constants.WORLD_WIDTH/2, 0, 20, 50));
-        ninja.addComponent(new CircleComponent(Constants.WORLD_WIDTH/2, Constants.WORLD_HEIGHT/2, 20));
-        ninja.addComponent(new DrawingComponent(255, 255, 255, 1));
-        ninja.addComponent(shapeRendererComponent);
-        ninja.addComponent(cameraComponent);
-        ninja.addComponent(new GravityComponent(10));
-        ninja.addComponent(new StayInScreenComponent());
-        ninja.addComponent(inputComponent);
-        ninja.addComponent(new VelocityComponent());
-        ninja.addToWorld();
-
-        //debug
-        Entity debugInfo = world.createEntity();
-        debugInfo.addComponent(new DebubInfoComponent());
-        debugInfo.addComponent(spriteBatchComponent);
-        debugInfo.addComponent(bitmapFontComponent);
-        debugInfo.addComponent(new DrawingComponent(255, 255, 255, 1));
-        debugInfo.addComponent(cameraComponent);
-        debugInfo.addComponent(inputComponent);
-        debugInfo.addToWorld();
+        worldManager = new WorldManager();
+        worldManager.createFlappyEntity();
+        worldManager.createDebugInfoEntity();
     }
 
     @Override
     public void render() {
-        prepareCameraAndComponents();
-
-        //world
-        world.setDelta(Gdx.graphics.getDeltaTime());
-        world.process();
+        worldManager.prepare();
+        worldManager.process();
     }
 
     @Override
@@ -112,19 +57,6 @@ public class FlappyGame implements ApplicationListener{
 
     @Override
     public void dispose() {
-        for(Disposable disposableComponent : componentsToDispose) {
-            disposableComponent.dispose();
-        }
-    }
-
-    private void prepareCameraAndComponents() {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        camera.update();
-
-        for(Preparable preparableComponent : componentsToPrepare) {
-            preparableComponent.prepare(camera);
-        }
+        worldManager.dispose();
     }
 }
---
+++ src/com/jak/flappy/world/WorldManager.java
@@ -0,0 +1,133 @@
+package com.jak.flappy.world;
+
+import com.artemis.Component;
+import com.artemis.Entity;
+import com.artemis.World;
+import com.artemis.managers.GroupManager;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Disposable;
+import com.badlogic.gdx.utils.ObjectMap;
+import com.jak.flappy.Constants;
+import com.jak.flappy.component.*;
+import com.jak.flappy.component.debug.DebubInfoComponent;
+import com.jak.flappy.component.graphic.*;
+import com.jak.flappy.system.GravitySystem;
+import com.jak.flappy.system.debug.DebugInfoSystem;
+import com.jak.flappy.system.graphic.DrawingSystem;
+import com.jak.flappy.system.graphic.LibgdxInputSystem;
+import com.jak.flappy.system.graphic.LibgdxScreenLimitedSystem;
+import com.jak.flappy.system.graphic.LibgdxVelocitySystem;
+
+/**
+ * Created by manu on 09/03/14.
+ */
+public class WorldManager {
+    private Array<Disposable> componentsToDispose;
+    private Array<Preparable> componentsToPrepare;
+    private World world;
+    private ObjectMap<String, Component> reusableComponents;
+    private Camera camera;
+
+    public WorldManager() {
+        componentsToDispose = new Array<Disposable>();
+        componentsToPrepare = new Array<Preparable>();
+        initializeWorld();
+        createReusableComponents();
+    }
+
+    public void createFlappyEntity() {
+        Entity flappy = world.createEntity();
+        //ninja.addComponent(new RectangleComponent(Constants.WORLD_WIDTH/2, 0, 20, 50));
+        flappy.addComponent(new CircleComponent(Constants.WORLD_WIDTH / 2, Constants.WORLD_HEIGHT / 2, 20));
+        flappy.addComponent(new DrawingComponent(255, 255, 255, 1));
+        flappy.addComponent(reusableComponents.get("shapeRenderer"));
+        flappy.addComponent(reusableComponents.get("camera"));
+        flappy.addComponent(new GravityComponent(10));
+        flappy.addComponent(new StayInScreenComponent());
+        flappy.addComponent(reusableComponents.get("input"));
+        flappy.addComponent(new VelocityComponent());
+        flappy.addToWorld();
+    }
+
+    public void createDebugInfoEntity() {
+        Entity debugInfo = world.createEntity();
+        debugInfo.addComponent(new DebubInfoComponent());
+        debugInfo.addComponent(reusableComponents.get("spriteBatch"));
+        debugInfo.addComponent(reusableComponents.get("font"));
+        debugInfo.addComponent(new DrawingComponent(255, 255, 255, 1));
+        debugInfo.addComponent(reusableComponents.get("camera"));
+        debugInfo.addComponent(reusableComponents.get("input"));
+        debugInfo.addToWorld();
+    }
+
+    public void prepare() {
+        prepareCamera();
+        prepareComponents();
+    }
+
+    public void process() {
+        world.setDelta(Gdx.graphics.getDeltaTime());
+        world.process();
+    }
+
+    public void dispose() {
+        for(Disposable disposableComponent : componentsToDispose) {
+            disposableComponent.dispose();
+        }
+    }
+
+    private void initializeWorld() {
+        world = new World();
+        world.initialize();
+        world.setManager(new GroupManager());
+        setWorldSystems();
+    }
+
+    private void setWorldSystems() {
+        world.setSystem(new LibgdxScreenLimitedSystem());
+        world.setSystem(new DrawingSystem());
+        world.setSystem(new GravitySystem());
+        //world.setSystem(new SpawnFlappySystem(2, cameraComponent));
+        world.setSystem(new LibgdxInputSystem());
+        world.setSystem(new LibgdxVelocitySystem());
+        world.setSystem(new DebugInfoSystem());
+    }
+
+    private void createReusableComponents() {
+        reusableComponents = new ObjectMap<String, Component>();
+        LibgdxCameraComponent cameraComponent = new LibgdxCameraComponent(Constants.WORLD_WIDTH, Constants.WORLD_HEIGHT);
+        camera = cameraComponent.getCamera();
+        BitmapFontComponent bitmapFontComponent = new BitmapFontComponent("calibri.png", "calibri.fnt", "fonts");
+        SpriteBatchComponent spriteBatchComponent = new SpriteBatchComponent();
+        ShapeRendererComponent shapeRendererComponent = new ShapeRendererComponent();
+
+        reusableComponents.put("camera", cameraComponent);
+        reusableComponents.put("spriteBatch", spriteBatchComponent);
+        reusableComponents.put("shapeRenderer", shapeRendererComponent);
+        reusableComponents.put("font", bitmapFontComponent);
+        reusableComponents.put("input", new LibgdxInputComponent());
+
+        componentsToDispose.add(bitmapFontComponent);
+        componentsToDispose.add(spriteBatchComponent);
+        componentsToDispose.add(shapeRendererComponent);
+
+        componentsToPrepare.add(shapeRendererComponent);
+        componentsToPrepare.add(spriteBatchComponent);
+    }
+
+    private void prepareComponents() {
+        for(Preparable preparableComponent : componentsToPrepare) {
+            preparableComponent.prepare(camera);
+        }
+    }
+
+    private void prepareCamera() {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+    }
+}
